bcfbff9f83088b42c0335474b40f80aa23e104f4
Darshil344X <84513686+Darshil344X@users.noreply.github.com>
2021-06-13 01:11:44 -0700

    Add files via upload

---
+++ Crossy-Road-0.5-main (1)/Crossy-Road-0.5-main/sketch.js
@@ -0,0 +1,40 @@
+var grid = 50;
+var width = 1366;
+var carGroup1,logGroup1;
+var grassHeight = 100;
+var gameState = "play";
+var carAnimation, logAnimation, playerAnimation;
+var school;
+function preload()
+{
+ 
+}
+
+function setup() {
+  createCanvas(1366,2700);
+  carGroup1 = new Group();
+  logGroup1 = new Group();
+  
+  player = new Player(width/2,height-25);
+ 
+   function keyPressed(){
+if (keyCode == UP_ARROW){
+  player.move(0,-2);
+}else if (keyCode == DOWN_ARROW){
+  player.move(0,2);
+}else if (keyCode == LEFT_ARROW){
+  player.move(-2,0);
+}else if (keyCode == RIGHT_ARROW){
+  player.move(2,0)
+
+}
+ }
+
+function draw() {
+  background("skyblue");
+ 
+ 
+
+  drawSprites();
+}
+
